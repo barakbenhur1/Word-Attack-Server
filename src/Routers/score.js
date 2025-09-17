23a091804033ac2fcfb27d6afe973b17806b50ee
barakbenhur1 <barakbenhur@gmail.com>
2025-09-17 22:37:56 +0300

    fix getPremium2

--- src/Routers/score.js
+++ src/Routers/score.js
@@ -116,6 +116,7 @@ async function premiumScore(email, res) {
   }
 
   member[0].premiumScore += 1;
+  member[1].save();
   member[1].premium.sort((a, b) => b.premiumScore - a.premiumScore);
   member[1].save();
 
